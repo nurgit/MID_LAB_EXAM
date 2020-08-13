f45f585999f620ece10dfcaa12bd31e5b30d61a2
Nur Mohammad <47762059+nurgit@users.noreply.github.com>
2020-08-13 12:19:52 +0600

    ligin working

--- app.js
+++ app.js
@@ -2,25 +2,25 @@
 var express 	= require('express');
 var exSession 	= require('express-session');
 var bodyParser 	= require('body-parser');
-//var login 		= require('./controller/login');
+var login 		= require('./controller/login');
 //var home 		= require('./controller/home');
 //var logout 		= require('./controller/logout');
 var app 		= express();
 
 //config
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 //app.use('/abc', express.static('assets'));
 //app.use('/abc/img', express.static('assets'));
 
 
 //middleware
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(exSession({secret: 'my secret value', saveUninitialized: true, resave: false}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(exSession({secret: 'my secret value', saveUninitialized: true, resave: false}));
 
 
-//app.use('/login', login);
-
+ app.use('/login', login);
+//app.use('/home', home);
 
 
 
