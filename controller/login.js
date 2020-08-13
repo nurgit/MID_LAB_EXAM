05385c368c6c48138f9963b73a49578b6d416e01
Nur Mohammad <47762059+nurgit@users.noreply.github.com>
2020-08-13 12:32:22 +0600

    T3M1F1

--- controller/login.js
+++ controller/login.js
@@ -6,22 +6,22 @@ router.get('/', function(req, res){
 	res.render('login/index');
 });
 
-// router.post('/', function(req, res){
-//
-// 	var user = {
-// 		uname: req.body.uname,
-// 		password: req.body.password
-// 	};
-//
-// 	userModel.validate(user, function(status){
-// 		if(status){
-// 			req.session.username = user.uname;
-// 			res.redirect('/home');
-// 		}else{
-// 			res.send('invalid username/password');
-// 		}
-// 	});
-//
-// });
+router.post('/', function(req, res){
+
+	var user = {
+		uname: req.body.uname,
+		password: req.body.password
+	};
+
+	userModel.validate(user, function(status){
+		if(status){
+			req.session.username = user.uname;
+			res.redirect('/home');
+		}else{
+			res.send('invalid username/password');
+		}
+	});
+
+});
 
 module.exports = router;
